fbd5e5e974eaac9c7cbe74be686b600423042f73
Arwin Marinta <arwinmarinta3@gmail.com>
2024-06-25 01:51:39 +0800

    config vercel json

--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
 const express = require("express"),
-  { PORT } = require('./config'),
+  { PORT } = require("./config"),
   bodyParser = require("body-parser"),
-  cors = require('cors'),
-  router = require('./routes'),
+  cors = require("cors"),
+  router = require("./routes"),
   /* rateLimiter = require('./middlewares/rate.limiter'), */
   app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-app.use(express.json())
-app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(cors());
 /* app.use(rateLimiter) */
-app.use('/api/v1', router)
+app.use("/api/v1", router);
 
-app.get('/test-connection', (req, res) => {
-  return res.status(200).send('ok')
-})
-
-app.listen(PORT, () => {
-  console.log(`Server is up and listening at port: ${PORT}`)
+app.get("/test-connection", (req, res) => {
+  return res.status(200).send("ok");
 });
 
+app.listen(PORT, () => {
+  console.log(`Server is up and listening at port: ${PORT}`);
+});
